c1517a74de0ac8cc2a56264a2baf833cdfdaccf5
Iskanderious <A.iskander07@gmail.com>
2026-09-20 11:43:29 +0500

    Исправлен package.json

--- src/router.ts
+++ src/router.ts
@@ -1,31 +1,14 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
 
-// Явно типизируем массив маршрутов
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'first',
-        component: () => import('./views/first.vue')
-    },
-    {
-        path: '/place',
-        name: 'place',
-        component: () => import('./views/place.vue'),
-    },
-    {
-        path: '/date',
-        name: 'date',
-        component: () => import('./views/date.vue'),
-    },
-    {
-        path: '/last',
-        name: 'last',
-        component: () => import('./views/last.vue')
-    }
+    { path: '/', name: 'first', component: () => import('./views/first.vue') },
+    { path: '/place', name: 'place', component: () => import('./views/place.vue') },
+    { path: '/date', name: 'date', component: () => import('./views/date.vue') },
+    { path: '/last', name: 'last', component: () => import('./views/last.vue') },
 ]
 
 const router = createRouter({
-    history: createWebHistory('/invite/'),
+    history: createWebHashHistory(),
     routes
 })
 
