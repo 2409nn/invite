1f6f19704ac4d32f7cdeb71eb3ca02e4e032c6b6
Iskanderious <A.iskander07@gmail.com>
2026-09-20 11:35:04 +0500

    Исправлен package.json

--- src/router.ts
+++ src/router.ts
@@ -25,7 +25,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory('/invite/'),
     routes
 })
 
